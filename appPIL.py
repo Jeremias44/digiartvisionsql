03d21612c7085acdef87f92d4a050978fbf74da6
Jeremias44 <jeremiaspombo@outlook.com>
2023-11-04 19:12:51 -0300

    feat: appPIL.py completo

--- appPIL.py
+++ appPIL.py
@@ -5,7 +5,47 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 from streamlit_drawable_canvas import st_canvas
-from pgadmin_connect_render import *
+import psycopg2
+
+
+# Se obtiene la URL de la base de datos desde las variables de entorno
+DATABASE_URL = 'postgres://digiartvisionsql_postrgressql_user:QbcjiMJNMV7nxMragK1Dxyyam2SMgvPn@dpg-cl36t99novjs73bcvcjg-a.oregon-postgres.render.com/digiartvisionsql_postrgressql'
+#DATABASE_URL = os.getenv(DATABASE_URL)
+# Establece una conexión a la base de datos
+conn = psycopg2.connect(DATABASE_URL)
+
+'''
+def crear_tabla_datos():
+    # Abre un cursor para interactuar con la base de datos
+    cursor = conn.cursor()
+    # Define la consulta SQL para crear la tabla si no existe
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS datos (
+        vector REAL[],
+        etiqueta INTEGER
+    );
+    """
+    # Ejecuta la consulta para crear la tabla
+    cursor.execute(create_table_query)
+    # Confirma los cambios en la base de datos
+    conn.commit()
+    # Cierra el cursor
+    cursor.close()
+'''
+def guardar_data(vector, etiqueta):
+    # Abre un cursor para interactuar con la base de datos
+    cursor = conn.cursor()
+    # Define la consulta de actualización SQL para asignar la calificación
+    insert_query = "INSERT INTO datos (vector, etiqueta) VALUES (ARRAY[%s], %s)"
+    # Crea una tupla con los datos a actualizar en la consulta
+    data = (vector, etiqueta)
+    # Ejecuta la consulta de actualización con los datos proporcionados
+    cursor.execute(insert_query, data)
+    # Confirma los cambios en la base de datos
+    conn.commit()
+    # Cierra el cursor
+    cursor.close()
+
    
 st.title('Modelo para reconocer números del 0 al 9 📚🚀💡👨‍💻')
 st.subheader('Este modelo se encuentra en proceso de entrenamiento 🏋️‍♂️ Podés jugar las veces que quieras y estarás ayudando a entrenarlo! 💪')
@@ -58,8 +98,6 @@ if st.checkbox('Iniciar Predicciones'):
 
     # Ahora, `predicted_number` contiene el número predicho por el modelo
     st.subheader(f'Valor detectado: {predicted_number}')
-
-
 
     # Aplana la imagen a un arreglo unidimensional
     input_image_flat = input_image.reshape(-1)
