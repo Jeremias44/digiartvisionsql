32dec68648f01af7d9404f0094d90ab952439fb2
Jeremias44 <jeremiaspombo@outlook.com>
2023-11-07 22:12:39 -0300

    feat: app..py presentación

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.title('DIGI ART VISION')
 
 
-st.markdown(f"[Acceder a Página Pincipal]('https://digiartvisionsql.streamlit.app/P%C3%A1gina_Principal')")
-st.markdown(f"[Acceder a Verificar Dibujos]('https://digiartvisionsql.streamlit.app/Verificar_Dibujos')")
+st.markdown("Desde el menú lateral podrás acceder a la pagina principal, y también a una página en la que podrás contribuir evaluando los últimos dibujos subidos a la aplicación")
+
 
 st.sidebar.markdown("### Contactá con el autor del sitio")
 st.sidebar.markdown("[Jeremías Pombo en LinkedIn](https://www.linkedin.com/in/jeremiaspombo/)")
