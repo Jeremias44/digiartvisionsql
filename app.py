9117028657305905fd7472cd1fd77784ed0b9fde
Jeremias44 <jeremiaspombo@outlook.com>
2023-11-07 22:29:43 -0300

    feat: cambio en app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title('DIGI ART VISION')
 
 st.info("## Te damos la bienvenida a DigiArtVision, la app de interacción con modelos de computer vision en la que podés contribuir con nuevos dibujos y con evaluación de dibujos realizados por otros usuarios")
 
-st.markdown("Desde el menú lateral podrás acceder a la pagina principal donde podrás dibujar, ver predicciones y alimentar la base de datos, y también a la página Verificar Dibujos, en la que podrás contribuir evaluando los últimos dibujos subidos a la aplicación")
+st.markdown("Desde el menú lateral podrás acceder a la sección principal donde podrás dibujar, ver predicciones y alimentar la base de datos. También podrás acceder a la sección Verificar Dibujos, en la que podrás contribuir evaluando los últimos dibujos subidos a la aplicación")
 
 
 st.sidebar.markdown("### Contactá con el autor del sitio")
