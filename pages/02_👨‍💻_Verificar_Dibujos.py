029211cb0908a4a218397a4909164f73da15cdc2
Jeremias44 <jeremiaspombo@outlook.com>
2023-11-07 22:24:40 -0300

    feat: actualiza sidebar

--- pages/02_👨‍💻_Verificar_Dibujos.py
+++ pages/02_👨‍💻_Verificar_Dibujos.py
@@ -16,13 +16,13 @@ conn = psycopg2.connect(DATABASE_URL)
 query = "SELECT * FROM datos"
 df = pd.read_sql_query(query, conn)
 
-st.sidebar.info(f"### En esta página podrás contribuir eliminando aquellos dibujos que considerás que fueron cargados con la etiqueta incorrecta, que fueron subidos sin información, o aquellos en los que el ojo humano no puede precisar exactamente de qué número se trata")
+st.sidebar.info(f"### En esta sección podrás contribuir eliminando aquellos dibujos que considerás que fueron cargados con la etiqueta incorrecta, o que no se puede saber con precisión de qué número se trata")
 etiqueta = st.sidebar.number_input('Para comenzar, ¿Cuál es el número que deseás verificar?:', 0, 9)
 limite = st.sidebar.selectbox("¿Cuántos dibujos deseás verificar?", (10,50,500,'todos'), index=3)
-st.sidebar.markdown(f"### Cantidad de Dibujos Registrados en la Base de Datos: {len(df)}")
+st.sidebar.info(f"### Gracias por contribuir a la limpieza y carga adecuada de los datos! 🏅")
+st.sidebar.markdown(f"### Dibujos Registrados en la Base de Datos: {len(df)}")
 next_power_of_10 = 10 ** len(str(len(df)))
-st.sidebar.info(f"## Gracias por contribuir a la limpieza y carga adecuada de los datos! 🏅")
-st.sidebar.markdown(f"## ¡¿Querés contribuir al desafío de alcanzar los {next_power_of_10} registros?! 😃")
+st.sidebar.markdown(f"## Te animás a contribuir al desafío de alcanzar los {next_power_of_10} registros?! 😃")
 
 st.sidebar.markdown("### Contactá con el autor del sitio")
 st.sidebar.markdown("[Jeremías Pombo en LinkedIn](https://www.linkedin.com/in/jeremiaspombo/)")
