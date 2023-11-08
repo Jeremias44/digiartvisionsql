3e14c803143f5b7aad5ae7889eb2d2090dba9035
Jeremias44 <jeremiaspombo@outlook.com>
2023-11-07 22:37:23 -0300

    feat: emois

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title('DIGI ART VISION')
+st.title('👁️ DIGI ART VISION 👁️')
 
-st.info("## Te damos la bienvenida a DigiArtVision, una app de interacción con modelos de computer vision")
+st.info("## Te damos la bienvenida a DigiArtVision, una app de interacción con modelos de computer vision 🤖")
 
-st.markdown("Desde el menú lateral podrás acceder a la sección principal donde podrás dibujar, ver predicciones y alimentar la base de datos. También podrás acceder a la sección Verificar Dibujos, en la que podrás contribuir evaluando los últimos dibujos subidos a la aplicación")
+st.markdown("Desde el menú lateral podrás acceder a la sección principal donde podrás dibujar ✏️, ver predicciones 🚀💡 y alimentar la base de datos 👨‍💻. También podrás acceder a la sección Verificar Dibujos 📝, en la que podrás contribuir 🏅 evaluando los últimos dibujos subidos a la aplicación")
 
 
 st.sidebar.markdown("### Contactá con el autor del sitio")
